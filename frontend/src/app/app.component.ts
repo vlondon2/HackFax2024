d666efb2d72d77de3dc64e45075ac97acc82cfa9
domhankle <domhankle03@gmail.com>
2024-02-16 14:16:48 -0500

    Base Angular app setup

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HomeComponent, SignInComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+
+
+}
---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+];
