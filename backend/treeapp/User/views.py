6afad26b63c7b915f5530d818b5177ff210adba6
Victor Londono <vlondon2@gmu.edu>
2024-02-16 18:54:00 -0500

    pycache wont get ignored KMS

--- backend/treeapp/User/views.py
+++ backend/treeapp/User/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import User
 
 # Create your views here.
+
+cosmeticsMap = {
+    'item': 'path'
+}
+
+@csrf_exempt
+def createUser(request):
+    try:
+        data = json.loads(request.body)
+
+        user = User.objects.create(
+            username=data['username'],
+            password=data['password'],
+            level=1,
+            xp=0,
+            lvlxp=100,
+            gold=0
+        )
+
+        user.save()
+
+        return JsonResponse({
+            'id':user.id,
+            'username':user.username,
+            'level':user.level,
+            'cosmeticPaths':[],
+            'tasks': []
+        })
+    except Exception as e:
+        return JsonResponse({"Message": str(e)}, status=400)
+    
+@csrf_exempt
+def getUser(request):
+    try:
+        data = json.loads(request.body)
+
+        user = User.objects.get(id=data['id'])
+
+        paths = []
+        cosmetics = separate(user.cosmetics)
+
+        while len(cosmetics) != 0:
+            paths.append()
+
+        return JsonResponse({
+            'id': user.id,
+            'username':user.username,
+            'level': user.level,
+            'cosmeticPaths': paths,
+            'tasks': user.getTasks
+        })
+    except User.DoesNotExist:
+        return JsonResponse({"Message": "User does not exist."}, status=404)
+    except Exception as e:
+        return JsonResponse({"Message": str(e)}, status=400)
+
+def join(array):
+    return ','.join(array)
+    
+def separate(array):
+    return array.split(",")
+    
