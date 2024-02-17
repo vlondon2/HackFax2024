1d4e766de790b6e3d3a59ba5d4de3ff4b7c74d9b
Dom Hankle <domhankle03@gmail.com>
2024-02-17 13:56:01 -0500

    adding crap

---
+++ frontend/src/app/components/create-account-dialog/create-account-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-account-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-account-dialog.component.html',
+  styleUrl: './create-account-dialog.component.scss'
+})
+export class CreateAccountDialogComponent {
+
+}
